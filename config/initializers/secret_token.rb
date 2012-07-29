958069ecdc940c9193ed019cf1646303a9f47361
cactis <cactis.lin@gmail.com>
2012-07-29 12:47:34 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = '21ad0cc513608cfb5f2554fed173633989e7348074c7573a104d0fc526ddbeee4a3754483888994b66909cc681abd56b1f087ea5d2cb2ffebc8f1590f3be0290'
